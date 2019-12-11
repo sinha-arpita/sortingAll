a3642b01eaabe99a1d68826e36017b9ffcc22c0e
Arpita Sinha <sinha.arpita@gmail.com>
2019-12-10 20:41:36 -0800

    done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,44 +5,34 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        min = arr[i]
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        min=arr[i]
+        # (hint, can do in 3 loc)
         for j in range (i+1,len(arr)):
-            if arr[j]<min:
-                min=arr[j]
-                smallest_index=j
-           # TO-DO: swap 
-        temp=arr[i]
-        arr[j]=arr[smallest_index]
-        arr[smallest_index]=temp
-    return arr         
+            if arr[j] <min :
+               min= arr[j]
+               cur_index= j
 
-print(selection_sort([7,4,9,0,1,2]))
+        # TO-DO: swap
+        temp= arr[i]
+        arr[i] = arr[cur_index]
+        arr[cur_index]= temp
+
+    return arr
+
+print ("Sorted array",selection_sort([5,3,8,2,1]))
 
 # TO-DO:  implement the Bubble Sort function below[5,2,9,0,4,1]
 def bubble_sort( arr ):
-    # outer loop that will decide how many times the process will be repeated, the number of passes, it will be len(arr-1
-    for i in range(0,len(arr)-1):
-        swap=0
-        #get inside the inner for loop which will compare the elements and if they are not at the right position it will swap
-        for j in range (0,len(arr)-1-i):
-            if arr[j]>arr[j+1]:
-                temp=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
-                swap=1
-        if swap==0:
-           break
-
+    swaps_occurred = True
+    while swaps_occurred:
+        swaps_occurred = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                # swap
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_occurred = True
     return arr
-print ("Sorted array",bubble_sort([5,3,8,2,1]))
-
-    
-
-
-
-
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
